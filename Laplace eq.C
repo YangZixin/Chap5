04e6273e029d2f89d9fe4f14e4761bb75a94c9d3
YangZixin <zixinyangcn@gmail.com>
2013-05-28 08:35:56 +0800

    Wrong about the position of the plates and grid

--- Laplace eq.C
+++ Laplace eq.C
@@ -4,22 +4,25 @@
 #include <fstream>
 using namespace std;
 
-const int LengthOfCapacitor = 1;
-const double GridInterval = 0.1;
-const int GridCheck = LengthOfCapacitor * 20 +1;
+const int LengthOfCapacitor = 2;
+const int GridInterval = 40;
+const int GridCheck = LengthOfCapacitor * GridInterval +1;
 const double PI = 3.14159;
 const double alpha = 2 / (1 + PI / LengthOfCapacitor);
+const int SetPlate = 0.3 * GridInterval;
 double potential[GridCheck][GridCheck];
 double temp_potential[GridCheck][GridCheck];
 double delta_V;
-double PermitErrorV = GridCheck * GridCheck * pow(10, -3);
+double PermitErrorV = GridCheck * GridCheck * pow(10, -5);
 
-void initializa_V()
+void initialize_V()
 {
-    for (int i = 0; i < GridCheck; i++)
+    for (int i = SetPlate; i < GridCheck-SetPlate; i++)
     {
-        potential[i][6] = 1;
-        potential[i][GridCheck-6] = -1;
+        potential[i][SetPlate] = 1;
+        temp_potential[i][SetPlate] = 1;
+        potential[i][GridCheck-SetPlate-1] = -1;
+        temp_potential[i][GridCheck-SetPlate-1] = -1;
     }
 }
 
@@ -30,16 +33,27 @@ void update_V()
     {
         for (int j = 1; j < GridCheck-1; j++)
         {
-            temp_potential[i][j] = (potential[i-1][j]+potential[i+1][j]+potential[i][j-1]+potential[i][j+1]) / 4;
-            delta_V += sqrt((potential[i][j]-temp_potential[i][j]) * (potential[i][j]-temp_potential[i][j]));
+            if (j != SetPlate && j != GridCheck-SetPlate-1)
+            {
+                temp_potential[i][j] = (potential[i-1][j]+potential[i+1][j]+potential[i][j-1]+potential[i][j+1]) / 4;
+                delta_V += sqrt((potential[i][j]-temp_potential[i][j]) * (potential[i][j]-temp_potential[i][j]));
+            }
+            else if (i < SetPlate || i >= GridCheck-SetPlate)
+            {
+                temp_potential[i][j] = (potential[i-1][j]+potential[i+1][j]+potential[i][j-1]+potential[i][j+1]) / 4;
+                delta_V += sqrt((potential[i][j]-temp_potential[i][j]) * (potential[i][j]-temp_potential[i][j]));
+            }
         }
     }
     for (int i = 1; i < GridCheck-1; i++)
     {
         for (int j = 1; j < GridCheck-1; j++)
         {
-            potential[i][j]=temp_potential[i][j];
+            
+                potential[i][j]=temp_potential[i][j];
         }
+        // potential[i][6] = 1;
+        // potential[i][GridCheck-7] = -1;
     }
 }
 
@@ -50,13 +64,48 @@ void update_V_SOR()
     {
         for (int j = 1; j < GridCheck-1; j++)
         {
+            if (j != SetPlate && j != GridCheck-SetPlate-1)
+            {
+                temp_potential[i][j] = (potential[i-1][j]+potential[i+1][j]+potential[i][j-1]+potential[i][j+1]) / 4;
+                potential[i][j] = alpha * (temp_potential[i][j] - potential[i][j]) + potential[i][j];
+                delta_V += alpha * sqrt((potential[i][j]-temp_potential[i][j]) * (potential[i][j]-temp_potential[i][j]));
+            }
+            else if (i < SetPlate || i >= GridCheck-SetPlate)
+            {
+                temp_potential[i][j] = (potential[i-1][j]+potential[i+1][j]+potential[i][j-1]+potential[i][j+1]) / 4;
+                potential[i][j] = alpha * (temp_potential[i][j] - potential[i][j]) + potential[i][j];
+                delta_V += alpha * sqrt((potential[i][j]-temp_potential[i][j]) * (potential[i][j]-temp_potential[i][j]));
+            }
+        }
+    }
+    for (int i = 1; i < GridCheck-1; i++)
+    {
+        for (int j = 1; j < GridCheck-1; j++)
+        {
+            
+                potential[i][j]=temp_potential[i][j];
+        }
+    }
+}
+
+
+/*void update_V_SOR()
+{
+    delta_V = 0;
+    for (int i = 1; i < GridCheck-1; i++)
+    {
+        for (int j = 1; j < GridCheck-1; j++)
+        {
+          if (j != 6 * LengthOfCapacitor && j != GridCheck-6 * LengthOfCapacitor - 1)
+            {
             temp_potential[i][j] = (potential[i-1][j]+potential[i+1][j]+potential[i][j-1]+potential[i][j+1]) / 4;
             potential[i][j] = alpha * (temp_potential[i][j] - potential[i][j]) + potential[i][j];
             delta_V += alpha * sqrt((potential[i][j]-temp_potential[i][j]) * (potential[i][j]-temp_potential[i][j]));
         }
+        }
     }
 
-}
+}*/
 
 
 int laplace_calculate()
@@ -84,7 +133,7 @@ int laplace_calculate_SOR()
 }
 int main()
 {
-	initializa_V();
+	initialize_V();
     int N_iter = laplace_calculate_SOR();
     cout<<N_iter<<endl;
     for (int i = 0; i < GridCheck; ++i)
